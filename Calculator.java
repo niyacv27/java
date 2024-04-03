eb28dca02ef57fdb22f3856195ee45ddb783ba56
niyacv27 <146043722+niyacv27@users.noreply.github.com>
2024-04-03 12:10:12 +0530

    package calpack

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+package Calpack;
+public class Calculator{
+      public void add(int a,int b){
+         System.out.println("SUM="+(a+b));
+      }
+      public void sub(int a,int b){
+         System.out.println("Difference="+(a-b));
+      }
+}         
