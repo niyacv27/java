26e80c819eabd796d9af44b580e48ac0f6698636
niyacv27 <146043722+niyacv27@users.noreply.github.com>
2024-04-03 12:13:36 +0530

    package import

---
+++ Testpack.java
@@ -0,0 +1,9 @@
+import Calpack.*;
+public class Testpack{
+  public static void main(String args[]){
+      Calculator c=new Calculator();
+      c.add(5,10);
+      c.sub(100,50);
+  }
+}      
+      
